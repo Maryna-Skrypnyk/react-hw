50c15bcabc4499e893b0226547d0e5e89210cd5f
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-08-10 13:38:12 +0300

    adds events-state-class, colorPicker, counter, dropdown, todoList

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## [homework-1](hw-1)
 
+## [events-state-class(counter, dropdown, color-picker, todo-list)](events-state-class)
+
 ## [homework-2](hw-2)
 
 ## [homework-2-phonebook](hw-2-phonebook)
---
+++ events-state-class/src/components/Counter/Value.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Value = ({ value }) => <span className="Counter__value">{value}</span>;
+
+Value.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default Value;
