03befa735689b6b2f8af1e3206bf780244ac6f47
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-09-11 11:34:28 +0300

    fix App

--- phonebook-hooks/src/index.js
+++ phonebook-hooks/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LocalizationContext from './context/localization';
 
 import 'modern-normalize/modern-normalize.css';
 import './styles/index.scss';
@@ -10,8 +9,6 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LocalizationContext>
-      <App />
-    </LocalizationContext>
+    <App />
   </React.StrictMode>,
 );
